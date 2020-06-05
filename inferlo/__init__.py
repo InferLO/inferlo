cc5b6b78d65f481dbaa7315acff54134fbc9f523
Dmytro Fedoriaka <fedimser@google.com>
2020-06-05 19:40:20 +0100

    Add setup.py

--- inferlo/__init__.py
+++ inferlo/__init__.py
@@ -1,11 +1,13 @@
 """Root InferLO package."""
 from inferlo.base import (
+    DiscreteDomain,
     Domain,
     Factor,
     DiscreteFactor,
     FunctionFactor,
     GenericGraphModel,
     GraphModel,
+    RealDomain,
     Variable,
 )
 from inferlo.forney import NormalFactorGraphModel
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as req_file:
+    requirements = [r.strip() for r in req_file.readlines()]
+
+setuptools.setup(
+    name="inferlo",
+    version="0.0.1",
+    author="The InferLO Developers",
+    description="Inference, Learning & Optimization with Graphical Models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache 2',
+    url="https://github.com/InferLO/inferlo",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=requirements,
+)
