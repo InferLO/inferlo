73f2ed71a5b5d891174865080771182c24751d63
mk <mkrechetow@gmail.com>
2020-07-06 00:56:15 +0300

    fixing style

--- inferlo/pairwise/optimization/convex_bounds_test.py
+++ inferlo/pairwise/optimization/convex_bounds_test.py
@@ -1,40 +1,43 @@
 import numpy as np
 
 from inferlo import PairWiseFiniteModel
-from inferlo.pairwise.optimization.convex_bounds import LPrelaxation
-from inferlo.pairwise.testing import grid_potts_model, tree_potts_model, \
-    line_potts_model
+from inferlo.pairwise.optimization.convex_bounds import lp_relaxation
+from inferlo.pairwise.testing import grid_potts_model
 
 
 def test_grid_4x4x2():
     model = grid_potts_model(4, 4, al_size=2, seed=0)
-    max_lh_gt = model.max_likelihood(algorithm='bruteforce')
+    max_lh_gt = model.max_likelihood(algorithm='path_dp')
     max_lh_gt_value = np.log(model.evaluate(max_lh_gt))
-    max_lh_value_relax = LPrelaxation(model)
-    assert (max_lh_gt_value <= max_lh_value_relax)
+    max_lh_lower_bound = lp_relaxation(model).lower_bound
+    max_lh_upper_bound = lp_relaxation(model).upper_bound
+    assert (max_lh_lower_bound <= max_lh_gt_value <= max_lh_upper_bound)
 
 
 def test_grid_3x3x4():
     model = grid_potts_model(3, 3, al_size=4, seed=0)
-    max_lh_gt = model.max_likelihood(algorithm='bruteforce')
+    max_lh_gt = model.max_likelihood(algorithm='path_dp')
     max_lh_gt_value = np.log(model.evaluate(max_lh_gt))
-    max_lh_value_relax = LPrelaxation(model)
-    assert (max_lh_gt_value <= max_lh_value_relax)
+    max_lh_lower_bound = lp_relaxation(model).lower_bound
+    max_lh_upper_bound = lp_relaxation(model).upper_bound
+    assert (max_lh_lower_bound <= max_lh_gt_value <= max_lh_upper_bound)
 
 
 def test_grid_2x2x10():
     model = grid_potts_model(2, 2, al_size=10, seed=0)
-    max_lh_gt = model.max_likelihood(algorithm='bruteforce')
+    max_lh_gt = model.max_likelihood(algorithm='path_dp')
     max_lh_gt_value = np.log(model.evaluate(max_lh_gt))
-    max_lh_value_relax = LPrelaxation(model)
-    assert (max_lh_gt_value <= max_lh_value_relax)
+    max_lh_lower_bound = lp_relaxation(model).lower_bound
+    max_lh_upper_bound = lp_relaxation(model).upper_bound
+    assert (max_lh_lower_bound <= max_lh_gt_value <= max_lh_upper_bound)
 
 
 def test_disconnected():
     model = PairWiseFiniteModel(size=4, al_size=5)
     model.add_interaction(0, 1, np.random.random(size=(5, 5)))
     model.add_interaction(2, 3, np.random.random(size=(5, 5)))
-    max_lh_gt = model.max_likelihood(algorithm='bruteforce')
+    max_lh_gt = model.max_likelihood(algorithm='path_dp')
     max_lh_gt_value = np.log(model.evaluate(max_lh_gt))
-    max_lh_value_relax = LPrelaxation(model)
-    assert (max_lh_gt_value <= max_lh_value_relax)
+    max_lh_lower_bound = lp_relaxation(model).lower_bound
+    max_lh_upper_bound = lp_relaxation(model).upper_bound
+    assert (max_lh_lower_bound <= max_lh_gt_value <= max_lh_upper_bound)
