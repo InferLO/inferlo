3f72ed35c1f6ecd335e504e9bffff5d3de598454
mk <mkrechetow@gmail.com>
2020-12-05 19:35:23 +0300

    fixing notation and descriptions

--- inferlo/forney/optimization/nfg_map_lp.py
+++ inferlo/forney/optimization/nfg_map_lp.py
@@ -16,49 +16,50 @@ map_lp_result = namedtuple('map_lp_result', ['upper_bound',
 
 
 def map_lp(model: NormalFactorGraphModel) -> map_lp_result:
-    """This function implements linear programming (LP) relaxation
-       of maximum a posteriori assignment problem (MAP) for
-       normal factor graph with finite alphabet.
+    """
+        This function implements linear programming (LP) relaxation
+        of maximum a posteriori assignment problem (MAP) for
+        normal factor graph with finite alphabet.
 
-       The goal of MAP estimation is to find most probable
-       assignment of original variables by maximizing probability
-       density function. For the case of pairwise finite model it
-       reduces to maximization of polynomial over finite
-       field.
+        The goal of MAP estimation is to find most probable
+        assignment of original variables by maximizing probability
+        density function. For the case of pairwise finite model it
+        reduces to maximization of polynomial over finite
+        field.
 
-       For every variable, we introduce Q non-negative belief variables
-       where Q is the size of the alphabet. Every such variable
-       is our 'belief' that variable at node takes particular value.
+        For every variable, we introduce Q non-negative belief variables
+        where Q is the size of the alphabet. Every such variable
+        is our 'belief' that variable at node takes particular value.
 
-       Analogously, for every factor we introduce Q^deg beliefs
-       where deg is a number of variables in factor.
+        Analogously, for every factor we introduce Q^deg beliefs
+        where deg is a number of variables in factor.
 
-       For both node and edge beliefs we require normalization
-       constraints: 1) for every variable, the sum of beliefs
-       equals one and 2) for every factor the sum of beliefs
-       equals one.
+        For both node and edge beliefs we require normalization
+        constraints: 1) for every variable, the sum of beliefs
+        equals one and 2) for every factor the sum of beliefs
+        equals one.
 
-       We also add marginalization constraint: for every factor,
-       summing factor beliefs over all except one node must equal
-       to the node belief at that node.
+        We also add marginalization constraint: for every factor,
+        summing factor beliefs over all except one node must equal
+        to the node belief at that node.
 
-       Finally we get a linear program and its solution is an
-       upper bound on the MAP value. We restore the lower bound
-       on MAP value as the solution of the dual relaxation.
+        Finally we get a linear program and its solution is an
+        upper bound on the MAP value. We restore the lower bound
+        on MAP value as the solution of the dual relaxation.
 
-       More details may be found in "MAP Estimation,
-       Linear Programming andBeliefPropagation with
-       Convex Free Energies" by Yair Weiss, Chen Yanover and Talya
-       Meltzer. https://arxiv.org/pdf/1206.5286.pdf
+        More details may be found in "MAP Estimation,
+        Linear Programming andBeliefPropagation with
+        Convex Free Energies" by Yair Weiss, Chen Yanover and Talya
+        Meltzer. https://arxiv.org/pdf/1206.5286.pdf
 
-       The output of the function is:
-       1) upper bound on MAP value (solution of LP)
-       2) lower bound on MAP value (dual solution)
-       3) Optimal values of factor beliefs
-       4) Optimal values of variable beliefs
-       5) Optimal values of dual variables that correspond to
+        The output of the function is:
+        1) upper bound on MAP value (solution of LP)
+        2) lower bound on MAP value (dual solution)
+        3) Optimal values of factor beliefs
+        4) Optimal values of variable beliefs
+        5) Optimal values of dual variables that correspond to
           normalization constraints
-       6) Optimal values of dual variables that correspond to
+        6) Optimal values of dual variables that correspond to
           marginalization constraints
     """
 
@@ -102,8 +103,9 @@ def map_lp(model: NormalFactorGraphModel) -> map_lp_result:
         for variable in range(len(factor_list[factor].var_idx)):
             for a in range(al_size):
                 expr = 0
-                var_vals = list(product(range(al_size),
-                                        repeat=len(factor_list[factor].var_idx)))
+                var_vals = list(
+                    product(range(al_size),
+                            repeat=len(factor_list[factor].var_idx)))
                 for x in var_vals:
                     temp = list(x)
                     temp[variable] = a
@@ -113,12 +115,14 @@ def map_lp(model: NormalFactorGraphModel) -> map_lp_result:
                 for x in var_vals:
                     expr += factor_beliefs[factor][x]
                 constraints += [expr ==
-                         variable_beliefs[factor_list[factor].var_idx[variable], a]]
+                                variable_beliefs[
+                                    factor_list[factor].var_idx[variable], a]]
 
     prob = cp.Problem(cp.Maximize(objective), constraints)
     prob.solve(solver=cp.SCS)
 
-    normal_dual_vars = [constraints[i].dual_value for i in range(len(factor_list))]
+    normal_dual_vars = [constraints[i].dual_value
+                        for i in range(len(factor_list))]
     marginal_dual_vars = [constraints[i].dual_value
                           for i in range(len(factor_list), len(constraints))]
     dual_objective = sum(normal_dual_vars)
--- inferlo/pairwise/optimization/map_lp.py
+++ inferlo/pairwise/optimization/map_lp.py
@@ -13,47 +13,48 @@ map_lp_result = namedtuple('map_lp_result', ['upper_bound',
 
 
 def map_lp(model: PairWiseFiniteModel) -> map_lp_result:
-    """This function implements linear programming (LP) relaxation
-       of maximum a posteriori assignment problem (MAP) for
-       pairwise graphical model with finite alphabet.
+    """
+        This function implements linear programming (LP) relaxation
+        of maximum a posteriori assignment problem (MAP) for
+        pairwise graphical model with finite alphabet.
 
-       The goal of MAP estimation is to find most probable
-       assignment of original variables by maximizing probability
-       density function. For the case of pairwise finite model it
-       reduces to maximization of quadratic function over finite
-       field.
+        The goal of MAP estimation is to find most probable
+        assignment of original variables by maximizing probability
+        density function. For the case of pairwise finite model it
+        reduces to maximization of quadratic function over finite
+        field.
 
-       For every node, we introduce Q non-negative belief variables
-       where Q is the size of the alphabet. Every such variable
-       is our 'belief' that variable at node takes particular value.
+        For every node, we introduce Q non-negative belief variables
+        where Q is the size of the alphabet. Every such variable
+        is our 'belief' that variable at node takes particular value.
 
-       Analogously, for every edge we introduce Q*Q pairwise beliefs.
+        Analogously, for every edge we introduce Q*Q pairwise beliefs.
 
-       For both node and edge beliefs we require normalization
-       constraints: 1) for every node, the sum of beliefs equals one
-       and 2) for every edge the sum of edge-beliefs equals one.
+        For both node and edge beliefs we require normalization
+        constraints: 1) for every node, the sum of beliefs equals one
+        and 2) for every edge the sum of edge-beliefs equals one.
 
-       We also add marginalization constraint: for every edge,
-       summing edge beliefs over one of the nodes must equal
-       to the node belief of the second node.
+        We also add marginalization constraint: for every edge,
+        summing edge beliefs over one of the nodes must equal
+        to the node belief of the second node.
 
-       Finally we get a linear program and its solution is an
-       upper bound on the MAP value. We restore the lower bound
-       on MAP value as the solution of the dual relaxation.
+        Finally we get a linear program and its solution is an
+        upper bound on the MAP value. We restore the lower bound
+        on MAP value as the solution of the dual relaxation.
 
-       More details may be found in "MAP Estimation,
-       Linear Programming andBeliefPropagation with
-       Convex Free Energies" by Yair Weiss, Chen Yanover and Talya
-       Meltzer. https://arxiv.org/pdf/1206.5286.pdf
+        More details may be found in "MAP Estimation,
+        Linear Programming andBeliefPropagation with
+        Convex Free Energies" by Yair Weiss, Chen Yanover and Talya
+        Meltzer. https://arxiv.org/pdf/1206.5286.pdf
 
-       The output of the function is:
-       1) upper bound on MAP value (solution of LP)
-       2) lower bound on MAP value (dual solution)
-       3) Optimal values of node beliefs
-       4) Optimal values of edge beliefs
-       5) Optimal values of dual variables that correspond to
+        The output of the function is:
+        1) upper bound on MAP value (solution of LP)
+        2) lower bound on MAP value (dual solution)
+        3) Optimal values of node beliefs
+        4) Optimal values of edge beliefs
+        5) Optimal values of dual variables that correspond to
           normalization constraints
-       6) Optimal values of dual variables that correspond to
+        6) Optimal values of dual variables that correspond to
           marginalization constraints
     """
 
@@ -77,7 +78,8 @@ def map_lp(model: PairWiseFiniteModel) -> map_lp_result:
         for a in range(model.al_size):
             for b in range(model.al_size):
                 J = model.get_interaction_matrix(edge[0], edge[1])
-                objective += J[a, b] * edge_beliefs[edge_list.index(edge)][a, b]
+                objective += J[a, b] * \
+                             edge_beliefs[edge_list.index(edge)][a, b]
 
     # normalization constraints
     for edge in edge_list:
@@ -102,7 +104,8 @@ def map_lp(model: PairWiseFiniteModel) -> map_lp_result:
     prob = cp.Problem(cp.Maximize(objective), constraints)
     prob.solve(solver=cp.SCS, eps=1e-8)
 
-    normal_dual_vars = [constraints[i].dual_value for i in range(len(edge_list))]
+    normal_dual_vars = [constraints[i].dual_value
+                        for i in range(len(edge_list))]
     marginal_dual_vars = [constraints[i].dual_value
                           for i in range(len(edge_list), len(constraints))]
     dual_objective = sum(normal_dual_vars)
